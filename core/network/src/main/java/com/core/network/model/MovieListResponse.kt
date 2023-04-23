e11f479ced5789357c7e9eab116c8ceed3a85977
Ravikant Sharma <ravikant.sharma@prepladder.com>
2023-04-23 18:37:11 +0530

    Dependencies adjusted and network module setup

---
+++ core/network/src/main/java/com/core/network/model/MovieListResponse.kt
@@ -0,0 +1,15 @@
+package com.core.network.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieListResponse(
+    @SerializedName("page")
+    val page: Int?,
+    @SerializedName("results")
+    val movieDTOS: List<MovieDTO?>?,
+    @SerializedName("total_pages")
+    val totalPages: Int?,
+    @SerializedName("total_results")
+    val totalResults: Int?
+)
